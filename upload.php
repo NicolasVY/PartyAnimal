027dea18dd1ef71d0727755b394fa247746677bb
Nikki <nikki.selen@gmail.com>
2020-03-09 09:47:10 +0100

    some new code

--- upload.php
+++ upload.php
@@ -1,38 +1,40 @@
 <?php
-	if (isset($_POST['submit'])) {
-	$file = $_FILES['file'];
+include('db.php');
 
-	$fileName = $_FILES['file']['name'];
-	$fileTmpName = $_FILES['file']['tmp_name'];
-	$fileSize = $_FILES['file']['size'];
-	$fileError = $_FILES['file']['error'];
-	$fileType = $_FILES['file']['type'];
-		
-	$fileExt = explode('.', $fileName);
-	$fileActualExt = strtolower(end($fileExt));
-	
-	$allowed = array('jpg', 'jpeg', 'png');
-	
-	if (in_array($fileActualExt, $allowed)) {
-		if ($fileError ===0){
-			if ($fileSize < 20000000) {
-				$fileNameNew = uniqid('', true). ".".$fileActualExt;
-				
-				$fileDestination = 'uploads/'.$fileNameNew;
-				
-				move_uploaded_file($fileTmpName, $fileDestination);
-				
-				header("location: index.php?uploadsucces");
-				
-			} else {
-				echo "Your file is to big!";
-			}
-				
-		} else{
-			echo "there was an error uploading your file!";
-		}
-	} else {
-		echo "You cannot upload files of this type!"
-	}
+// Check if user is logged in using the session variable
+if ( $_SESSION['logged_in'] != 1 ) {
+  $_SESSION['message'] = "You must log in before viewing your profile page!";
+  header("location: error.php");    
+}
 
+if (isset($_POST['submit'])) {
+    $file = $_FILES['file'];
+
+    $fileName = $file['name'];
+    $fileTmpName = $file['tmp_name'];
+    $fileSize = $file['size'];
+    $fileError = $file['error'];
+    $fileType = $file['type'];
+
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+
+    if (in_array($fileActualExt, $allowed)) {
+        if ($fileError === 0) {
+            if ($fileSize < 1000000) {
+                $fileNameNew = uniqid('', true).".".$fileActualExt;
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                header("Location: user.php");
+            } else {
+                echo "Your file is too big!";
+            }
+        } else {
+            echo "There was an error uploading your file!";
+        }
+    } else {
+        echo "You cannot upload files of this type!";
+    }
 }

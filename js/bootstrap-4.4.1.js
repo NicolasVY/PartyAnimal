00ba9a88ee0667e01546779748b249bebc4e3eaf
Steven Mogensen <stevenholmmogensen@gmail.com>
2020-03-11 10:09:26 +0100

    .

--- js/bootstrap-4.4.1.js
+++ js/bootstrap-4.4.1.js
@@ -4520,4 +4520,8 @@
 })));
 
 
+if(empty($_POST['agree']) || $_POST['agree'] != 'agree') {
+    echo 'Please indicate that you have read and agree to the Terms and Conditions and Privacy Policy';
+}
+
 //# sourceMappingURL=bootstrap.js.map
